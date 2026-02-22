cabe0e30bdf4dc74f8d4dcddc4fa19f6f6260f1f
selalelo moakamelo <117808094+Selalelo@users.noreply.github.com>
2026-02-22 11:58:06 +0000

    update

--- app/app.py
+++ app/app.py
@@ -555,4 +555,5 @@ def startup():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))  # Render sets PORT automatically
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
